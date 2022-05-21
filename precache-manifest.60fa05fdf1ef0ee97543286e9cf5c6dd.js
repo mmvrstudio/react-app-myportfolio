381014b0054a010bcd72e5141e51562dd9696525
Mariana Marques <65464431+TheQueenofEverything@users.noreply.github.com>
2022-05-21 11:35:03 -0400

    Updates

---
+++ precache-manifest.60fa05fdf1ef0ee97543286e9cf5c6dd.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4baf9cbbf218d3f8e71392f5acbfddc",
+    "url": "/react-app-myportfolio/index.html"
+  },
+  {
+    "revision": "6013a4e68087d9b359ba",
+    "url": "/react-app-myportfolio/static/css/main.4b247cc3.chunk.css"
+  },
+  {
+    "revision": "9dbf2033ff3544a033a3",
+    "url": "/react-app-myportfolio/static/js/2.4728d251.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/react-app-myportfolio/static/js/2.4728d251.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "234fabeb178032a3c2b6",
+    "url": "/react-app-myportfolio/static/js/3.f94c5e6e.chunk.js"
+  },
+  {
+    "revision": "6013a4e68087d9b359ba",
+    "url": "/react-app-myportfolio/static/js/main.9c6cd48f.chunk.js"
+  },
+  {
+    "revision": "ccfd5c6a0b1a9a2c262a",
+    "url": "/react-app-myportfolio/static/js/runtime-main.4baa0199.js"
+  },
+  {
+    "revision": "d6c04742d9160faafd65432a33b77262",
+    "url": "/react-app-myportfolio/static/media/Alien.d6c04742.png"
+  },
+  {
+    "revision": "7c2b49da413d750e9b5ff33a22e8ba38",
+    "url": "/react-app-myportfolio/static/media/Book_Search.7c2b49da.png"
+  },
+  {
+    "revision": "762efcf91a1878d92de2563d718ba815",
+    "url": "/react-app-myportfolio/static/media/Guitar_Shop.762efcf9.png"
+  },
+  {
+    "revision": "8ceec6cf2c49123c358ccfe1be2b9abc",
+    "url": "/react-app-myportfolio/static/media/Insomnia.8ceec6cf.png"
+  },
+  {
+    "revision": "aea0f0486d1a80fbbe30a990bbfa8238",
+    "url": "/react-app-myportfolio/static/media/MySQL.aea0f048.png"
+  },
+  {
+    "revision": "c3fe29e5a4b4a8e27cd8ca8468d11240",
+    "url": "/react-app-myportfolio/static/media/MyTechBlog.c3fe29e5.png"
+  },
+  {
+    "revision": "938432ca660577a967cbcce20bcd2ec6",
+    "url": "/react-app-myportfolio/static/media/Quiz_Javascript.938432ca.png"
+  },
+  {
+    "revision": "b72e7de6a4e8885aeed9d3bf4d10efe2",
+    "url": "/react-app-myportfolio/static/media/ResumeMarianaMarques2022.b72e7de6.pdf"
+  },
+  {
+    "revision": "8e0a17632e614da29eca0aad3ba22952",
+    "url": "/react-app-myportfolio/static/media/SH_budgettracker.8e0a1763.png"
+  },
+  {
+    "revision": "b609aaee4c893c61e35a3f7d006598f6",
+    "url": "/react-app-myportfolio/static/media/Solidity.b609aaee.png"
+  },
+  {
+    "revision": "ed7f15c8e538f08e25e4d59a4aea2309",
+    "url": "/react-app-myportfolio/static/media/Team_Profile.ed7f15c8.png"
+  },
+  {
+    "revision": "2770f044e2d16f5151774ac9c1b7c4e8",
+    "url": "/react-app-myportfolio/static/media/Weather_Dashboard.2770f044.png"
+  },
+  {
+    "revision": "c32e24f32f90fb1eb7bfb173ce33237e",
+    "url": "/react-app-myportfolio/static/media/me.c32e24f3.png"
+  },
+  {
+    "revision": "ff61d8ae56f9ccae6d3d04e9a02f3dbf",
+    "url": "/react-app-myportfolio/static/media/me_transparency.ff61d8ae.png"
+  }
+]);
